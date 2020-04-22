c35ecceb2c0a1030e84101a4b5cbb3d712e81c69
曾立杰 <kobe830711@gmail.com>
2020-04-22 21:29:47 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,66 @@
 var gulp = require('gulp');
-var prompt = require("gulp-prompt");
+var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
+var fileinclude = require('gulp-file-include');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 
 
+gulp.task('concatjs', function () {
+    gulp.src('js/*.js').pipe(gulp.dest('assets/js'));
+})
+
+gulp.task('concatcss',function () {
+    gulp.src('css/*.css').pipe(gulp.dest('assets/css'));
+})
 
 
-//樣式 sass style
-// var styles = require('./gulp/scripts/style.js');
-var styles = require('./gulp/scripts/stylus.js');
-
-
-
-// 刷新瀏覽器並同步
-var browerSync = require('./gulp/task/browerSync.js');
-// 選擇 1.html 使用app/html   2.pug 使用app/pug  3. all 使用全部
-var selectMoudule = new browerSync('html') // html or pug or all
-
-
-
-//打包
-
-var build = require('./gulp/scripts/copy_dist.js');
-// var build = require('./gulp/scripts/connect-php.js');
-
-
-//壓縮
-var autoprefixer = require("./gulp/scripts/autoprefixer.js");
-
-
-
-
-//==================
-// 執行指令
-//==================
-
-
-// 一般開發
-// gulp.task('default', ['styles', 'static', 'libs']);
-// gulp.task('common', ['styles', 'static']);
-
-//server 專案
-gulp.task('server', ['styles', 'connect-sync']);
-
-//打包專案
-gulp.task('build', ['dist']);
-//壓縮專案
-
-//清除檔案
-
-// gulp.task('clear', ['clean']);
-
-//test function
-// gulp.task('test', ['styles']);
-
-
-
-
-
-gulp.task('selecttask', function () {
-    
-    //三種模式用變數帶出來    
-    var  task_mission = ['all' , 'dev' , 'clear' , 'style' ,'stylus' , 'comp']
-
-    return gulp.src('./gulpfile.js')
-        .pipe(prompt.prompt({
-            type: 'checkbox',
-            name: 'task',
-            message: '你想執行那個任務？（ 請按空白鍵選擇 ）',
-            choices: task_mission
-        }, function (res) {
-            var selectedTask = res.task;
-            gulp.start(selectedTask);
-            // console.log('選中:', res);
-        }));
+//
+gulp.task('sass' ,function () {
+    gulp.src('sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css/'))
 });
 
-// 執行模式  會相對應上方的task_mission 變數
-gulp.task('all', ['sass', 'static', 'libs']);
-gulp.task('dev', ['sass', 'static']);
-gulp.task('clear', ['clean']);
-gulp.task('style', ['sass' , 'watch']);
-gulp.task('stylus', ['stylus_follow']);
-gulp.task('comp', ['autoprefixer']);
-
-//執行
-gulp.task('default', ['selecttask']);
-
+gulp.task('minicss',['concatcss'], function () {
+    gulp.src('css/*.css')
+        //壓縮
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        //完成搬家
+        .pipe(gulp.dest('css/mini/'));
+});
 
 
-// gulp.task('pugs', ['compilePug']);
+gulp.task('watch' ,function () {
+    gulp.watch('sass/*.scss',['sass']);
+    gulp.watch('css/*.css',['concatcss']);
+});
+
+
+gulp.task('fileinclude', function() {
+    gulp.src(['contactus.html'])
+      .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+      }))
+      .pipe(gulp.dest('./app'));
+  });
+
+
+
+  gulp.task('default', function () {
+    browserSync.init({
+        server: {
+            //根目錄
+            baseDir: "./",
+            index: "tweenmax.html"
+        }
+    });
+
+    gulp.watch(["sass/*.scss", "sass/**/*.scss"], ['sass']).on('change', reload);
+    gulp.watch(["*.html"]).on('change', reload);
+    gulp.watch(["js/*.js"]).on('change', reload);
+});
