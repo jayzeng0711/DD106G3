1280885b5116180c9460916ac6da89ea76a845f0
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2020-05-13 15:43:37 +0800

    接資料庫列出管理員資料

--- php/admin.php
+++ php/admin.php
@@ -1,8 +1,11 @@
 <?php
+try{
     require_once("connectdd106g3.php");
-
-    $sql = "select * from `admin`";
+    $sql = "select * from admin";
     $admin = $pdo->query($sql);
-    $adminRow = $admin->fetch(PDO::FETCH_ASSOC);
-    echo json_encode($adminRow);
-?>
+    $adminRows = $admin->fetchAll(PDO::FETCH_ASSOC);
+}catch(PDOException $e){
+    echo "Debuging: ", $e->getMessage(), "<br>";
+    echo "Detailing: ", $e->getLine(), "<br>";
+}
+?>   
