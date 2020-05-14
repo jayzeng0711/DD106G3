35d421efb9e9660abd52052ae8b8a5383c0f0db8
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2020-05-14 16:11:56 +0800

    更改接收資料的方式 變成ajax

--- php/admin.php
+++ php/admin.php
@@ -1,11 +1,24 @@
 <?php
 require_once("connectdd106g3.php");
-$sql = "select * from `admin` where adminNo=:adminNo";
+$sql = "select * from `admin`";
 $admin = $pdo->query($sql);
-$admin->execute();
 $adminRows = $admin->fetchAll(PDO::FETCH_ASSOC);
 
-echo json_encode($adminRows);
-
-?>
+if(isset($_GET['data']) && $_GET['data'] == 'check'){
  
+    foreach($adminRows as $adminRow){
+        ?>
+        <tr>
+        <td><?=$adminRow["adminNo"]?></td>
+        <td><?=$adminRow["adminId"]?></td>
+        <td><?=$adminRow["adminPsw"]?></td>
+        <td><?=$adminRow["adminName"]?></td>
+        <td><?=$adminRow["adminAuthority"]?></td>
+        <td>
+            <a href=""><i class="fa fa-pencil" aria-hidden="true"></i></a>
+        </td>
+        </tr>
+        <?php    
+        }
+   }
+?>
