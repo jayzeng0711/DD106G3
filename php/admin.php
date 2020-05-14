87309cc22a2570704f6900341aeefc8ad1e56074
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2020-05-14 14:14:17 +0800

    try to figuering it out do not push IT

--- php/admin.php
+++ php/admin.php
@@ -1,11 +1,11 @@
 <?php
-try{
-    require_once("connectdd106g3.php");
-    $sql = "select * from admin";
-    $admin = $pdo->query($sql);
-    $adminRows = $admin->fetchAll(PDO::FETCH_ASSOC);
-}catch(PDOException $e){
-    echo "Debuging: ", $e->getMessage(), "<br>";
-    echo "Detailing: ", $e->getLine(), "<br>";
-}
-?>   
+require_once("connectdd106g3.php");
+$sql = "select * from `admin` where adminNo=:adminNo";
+$admin = $pdo->query($sql);
+$admin->execute();
+$adminRows = $admin->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($adminRows);
+
+?>
+ 
