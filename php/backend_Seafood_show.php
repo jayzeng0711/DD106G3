3fbb900abfa6754853ddf6587f63ea0a9ab72913
qwq1793 <qwq1793@gmail.com>
2020-05-23 15:49:37 +0800

    後台海鮮管理修改功能完成

--- php/backend_Seafood_show.php
+++ php/backend_Seafood_show.php
@@ -5,7 +5,7 @@ $errMsg = "";
 try {
     require_once("connectdd106g3.php");
 
-    $sql = "select * from `seafood`";
+    $sql = "SELECT * FROM `seafood` s JOIN cook c on s.`cookNo` = c.`cookNo` ORDER BY `s`.`seafoodNo`";
     $seafood = $pdo->query($sql);
     $seaFoodRows = $seafood->fetchAll(PDO::FETCH_ASSOC);    
     echo json_encode($seaFoodRows);
